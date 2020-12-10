77ab28b52338b52828afed03f8e6c0be824ed5d9
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-10 09:57:20 -0500

    updated take quiz button layout and mobile style

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,42 +2,40 @@
 var generateBtn = document.querySelector("#playQuiz");
 
 var quizData = [
-  { question: "What color is the sky?", answer: "Blue" },
-  { question: "What color is the grass?", answer: "Green" },
-  { question: "What is the capitol of Georgia?", answer: "Atlanta" },
+    { question: "What color is the sky?", answer: "Blue" },
+    { question: "What color is the grass?", answer: "Green" },
+    { question: "What is the capitol of Georgia?", answer: "Atlanta" },
 ];
 
 console.log(quizData[2].question); //Validating syntax
 
 function playQuiz() {
-  // Hide "Take Quiz" Button
-  document.getElementById("playQuiz").style.display = "none";
+    // Hide "Take Quiz" Button
+    document.getElementById("playQuiz").style.display = "none";
 
-  // Show Question text
-  document.getElementById("questionBox").style.display = "flex";
+    // Show Question text
+    document.getElementById("questionBox").style.display = "flex";
 
-  // Declare Variables
-  var userScore = [0, 0];
-  var gameTimer = 100;
+    // Declare Variables
+    var userScore = [0, 0];
+    var gameTimer = 100;
+    var userAnswer = "";
 
-  // Loop over length of quizData array
-  for (var i = 0; i < quizData.length; i++) {
-    // Show Question in textbox
-    
-
-    for (var j = 0; j < 5; j++) {
+    // Loop over length of quizData array
+    for (var i = 0; i < quizData.length; i++) {
+        // Show Question in textbox
         document.getElementById("questionBox").value = quizData[i].question;
-      for (var k = 0; k < 2; k++) {
-        console.log(j);
-        console.log(k);
-        
-      }
+        do {
+            //SOMETHING?
+        } while (userAnswer != null);
     }
-    document.getElementById("questionBox").value = "Game Over";
+}
+
+document.getElementById("questionBox").value = "Game Over";
 
     // Switch on answer button response
   }
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", playQuiz);

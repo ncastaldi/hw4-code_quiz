c8505663b30f4db87e79ad086947574cbc2b251d
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-14 17:57:21 -0500

    updated quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,37 @@
 /* Declare Variables */
 var generateBtn = document.querySelector("#playQuiz");
 
-var quizData = [
-    { question: "What color is the sky?", answer: "Blue" },
-    { question: "What color is the grass?", answer: "Green" },
-    { question: "What is the capitol of Georgia?", answer: "Atlanta" },
+const quizData = [
+    {
+        question: "Who invented JavaScript?",
+        answers: {
+            a: "Douglas Crockford",
+            b: "Sheryl Sandberg",
+            c: "Brendan Eich",
+            d: "Johnathan Watson"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What do we do with arrays?",
+        answers: {
+            a: "Iterate over them",
+            b: "Ignore them",
+            c: "Stringify them"
+            d: "Console log them"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript??",
+        answers: {
+            a: "<scripting>",
+            b: "<script>",
+            c: "<javascript>",
+            d: "<js>"
+        },
+        correctAnswer: "b"
+    }
 ];
 
 console.log(quizData[2].question); //Validating syntax
@@ -17,17 +44,15 @@ function playQuiz() {
     document.getElementById("questionBox").style.display = "flex";
 
     // Declare Variables
-    var userScore = [0, 0];
-    var gameTimer = 100;
+    var userScore = [0, 0]; // TODO: Replace with local storage
+    var gameTimer = 100; //TODO: Replace with SetInterval function
     var userAnswer = "";
 
-    // Loop over length of quizData array
-    // TO DO: Create timer interval
 }
 
 document.getElementById("questionBox").value = "Game Over";
 
-    // Switch on answer button response
+// Switch on answer button response --> Eventlistener on button group
 
 
 

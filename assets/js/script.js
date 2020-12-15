88fa9697a89dda969791d7c0b66fe394e3e504f0
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-15 14:48:43 -0500

    updated js logic to progress through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 /* Declare DOM Variables */
-var startPage = document.getElementById("startPage");
+var welcomeMsg = document.getElementById("welcomeMsg");
 var playQuizBtn = document.getElementById("playQuiz");
 var quizQuestionBox = document.getElementById("quizQuestions")
 var quizAnswerBox = document.getElementById("quizAnswers");
-
 
 /* Declare Javascript Variables */
 var questionIndex = 0;
@@ -11,8 +10,8 @@ var questionIndex = 0;
 const quizData = [
     {
         question: "Commonly used data type Do Not include:---",
-        choices: ["strings", "booleance", "alerts", "numbers"],
-        answer: "alerts"
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
     },
     {
         question: "The condition in an if/else statement is enclosed within:---",
@@ -38,31 +37,39 @@ const quizData = [
 
 /* Define functions */
 function playQuiz() {
-    //TEST
-    console.log(quizData[1].choices[0]);
-    //TEST
-
     // Hide  Welcome Message and "Take Quiz" Button
-    startPage.setAttribute("class", "hide");
+    welcomeMsg.setAttribute("class", "hide");
 
     // Show Question Box and Answer Buttons
     quizQuestionBox.setAttribute("class", "show");
     quizAnswerBox.setAttribute("class", "show");
 
+    displayQuestion();
+}
+
+function displayQuestion() {
     // Iterate over Question and Answer Arrays
     quizQuestionBox.textContent = quizData[questionIndex].question;
     for (var i = 0; i < 4; i++) {
         var answerBtn = document.createElement("button");
         answerBtn.textContent = quizData[questionIndex].choices[i];
-        answerBtn.setAttribute("style", "btn btn-success");
+        answerBtn.setAttribute("data-id", i);
         quizAnswerBox.appendChild(answerBtn);
+        answerBtn.addEventListener("click", function () {
+            if (this.textContent === quizData[questionIndex].answer) {
+                // notify correct
+                questionIndex++;
+                quizAnswerBox.textContent = "";
+                displayQuestion();
+            } else {
+                // subtract time from timer
+                questionIndex++;
+                quizAnswerBox.textContent = "";
+                displayQuestion();
+            }
+        })
     }
-}
-
-function nextQuestion(){
-    console.log("working?");
 }
 
 /* Register Event Listeners */
 playQuizBtn.addEventListener("click", playQuiz);
-quizAnswerBox.addEventListener("click", nextQuestion);
